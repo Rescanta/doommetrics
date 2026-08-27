df6f7537165db8e37acb3999525f999a9c556caa
Rescanta <81989780+Rescanta@users.noreply.github.com>
2026-08-27 21:24:17 +0300

    Rename package and project identifiers from tnamai to rescanta

--- src/main/java/com/rescanta/doommetrics/DoomMetricsOverlay.java
+++ src/main/java/com/rescanta/doommetrics/DoomMetricsOverlay.java
@@ -1,4 +1,4 @@
-package com.tnamai.doommetrics;
+package com.rescanta.doommetrics;
 
 import java.awt.Dimension;
 import java.awt.Graphics2D;
--- src/test/java/com/rescanta/doommetrics/DoomMetricsPluginTest.java
+++ src/test/java/com/rescanta/doommetrics/DoomMetricsPluginTest.java
@@ -1,4 +1,4 @@
-package com.tnamai.doommetrics;
+package com.rescanta.doommetrics;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
